60c3758d62112296a92199ccc8fcc8f55f166719
Phạm Khang Bình <82141543+khangbinhdl@users.noreply.github.com>
2021-10-08 19:00:14 +0700

    Update youtube_sort.js

--- youtube_sort.js
+++ youtube_sort.js
@@ -1,31 +1,28 @@
 // ==UserScript==
 // @name         Sort youtube videos
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Bring back the sort videos feature for Music Youtube channels
-// @author       Loi
+// @author       Binh
 // @match        https://www.youtube.com/c/*/video*
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
 // @grant        none
 // ==/UserScript==
 
-// discussion: https://www.reddit.com/r/youtube/comments/k4s1cm/did_youtube_remove_the_feature_to_sort_videos_by/
-
 (function () {
   'use strict';
   const sortMenu = document.querySelector('#sort-menu');
-  sortMenu.style.background = 'white';
+  sortMenu.style.background = 'dark';
   sortMenu.style.padding = '8px';
   sortMenu.style.borderRadius = '2px';
-  sortMenu.style.boxShadow = 'var(--shadow-elevation-2dp_-_box-shadow)';
+  // sortMenu.style.boxShadow = 'var(--shadow-elevation-2dp_-_box-shadow)';
   sortMenu.style.fontSize = '1.2em';
+  sortMenu.style.color = 'white'
   sortMenu.innerHTML = `
-  <label for="sort-select">Sort by:</label>
-  <select name="sort-select" id="sort-select" onchange="location = this.value;">
-    <option disabled selected value> -- select an option -- </option>
-    <option value="?view=0&sort=p&flow=grid">Most popular</option>
-    <option value="?view=0&sort=da&flow=grid">Date added (oldest)</option>
-    <option value="?view=0&sort=dd&flow=grid">Date added (newest)</option>
+  <label for="sort-select"">Sắp xếp theo:</label>
+ <select name="sort-select" id="sort-select" onchange="location = this.value;">
+    <option disabled selected value>  </option>
+    <option value="?view=0&sort=p&flow=grid">Phổ biến nhất</option>
+    <option value="?view=0&sort=da&flow=grid">Ngày thêm (cũ nhất)</option>
+    <option value="?view=0&sort=dd&flow=grid">Ngày thêm (mới nhất)</option>
   </select>
     `;
 })();
